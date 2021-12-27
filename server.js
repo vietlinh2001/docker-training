cf85cb57139ee14b84d8c29ea607e69ef7a6bc0c
Nguyen Cao Cuong <nguyencaocuong31082001@gmail.com>
2021-12-27 12:37:11 +0700

    ignoring unnecessary files

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const http = require('http');
 
+const query = async () => {
+
+const { Client } = require('pg');
+const client = new Client({ user: 'postgres', password: 'root', host: 'db' });
+await client.connect();
+const res = await client.query('SELECT $1 as message', ['Hello world!']);
+
+await client.end();
+
+return res.rows[0];
+
+}
+
 const server = http.createServer((request, response) => {
 
-    response.writeHead(200, {
-        'content-type': 'text/plain',
-    });
+    query().then((result) => {
+         response.writeHead(200, {
+             'content-type': 'application/json',
+         });
 
-    response.end('Hello World');
+         response.end(JSON.stringify(result));
+    });
 });
 
 
